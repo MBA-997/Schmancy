9583c793fdca6a00344337e7db275aa64a3ab899
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-08-27 14:44:36 +0500

    Added navigation and integrated fonts

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Schmancy',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.pink,
+          colorScheme: ColorScheme.fromSwatch(accentColor: Colors.grey),
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              titleMedium: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
       home: const CategoriesScreen(),
     );
   }
