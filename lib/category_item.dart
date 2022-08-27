091714c214a0611d5ff71505a5f37f0ba18c6fd0
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-08-27 14:44:02 +0500

    Added Title to Grid

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -21,6 +21,10 @@ class CategoryItem extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
           borderRadius: BorderRadius.circular(15)),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
     );
   }
 }
