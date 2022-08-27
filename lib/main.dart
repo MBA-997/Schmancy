01a91ce0c271b3e05e6286397ca687e15c53ccf7
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-08-27 21:58:54 +0500

    Removed custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './categories_screen.dart';
+
+import 'screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,22 +16,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Schmancy',
       theme: ThemeData(
-          primarySwatch: Colors.pink,
-          colorScheme: ColorScheme.fromSwatch(accentColor: Colors.grey),
-          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: const TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              bodyText2: const TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              titleMedium: const TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold))),
-      home: const CategoriesScreen(),
+        primarySwatch: Colors.pink,
+        colorScheme: ColorScheme.fromSwatch(accentColor: Colors.grey),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+      ),
+      // fontFamily: 'Raleway',
+      // textTheme: ThemeData.light().textTheme.copyWith(
+      //     bodyText1: const TextStyle(
+      //       color: Color.fromRGBO(20, 51, 51, 1),
+      //     ),
+      //     bodyText2: const TextStyle(
+      //       color: Color.fromRGBO(20, 51, 51, 1),
+      //     ),
+      //     titleSmall: const TextStyle(
+      //         fontSize: 20,
+      //         // fontFamily: 'RobotoCondensed',
+      //         fontWeight: FontWeight.bold))
+      //         ),
+      routes: {
+        '/': (ctx) => const CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => const CategoryMealsScreen()
+      },
     );
   }
 }
